5dc1d984a51611b45b321e5c2b3cb44dfc6e2ba8
nikola <hannah.melton63@gmail.com>
2020-04-10 15:48:19 -0700

    Initial commit, transferring files to rails 5

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,66 @@
+class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+  layout "portfolio"
+  access all: [:show, :index, :vue], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def vue
+    @vue_portfolio_items = Portfolio.vue
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show 
+  end
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed'}
+    end
+  end
+
+  private
+    def portfolio_params
+      params.require(:portfolio).permit(:title, 
+                                        :subtitle, 
+                                        :body, 
+                                        technologies_attributes: [:name]
+                                       )
+    end
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+end
