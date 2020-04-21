cbe27f321c1274f642064c2ca6ce9b3972238451
nikola <hannah.melton63@gmail.com>
2020-04-21 13:25:00 -0700

    built out initial form and partial call for comments along with controller actions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,31 +3,26 @@ class BlogsController < ApplicationController
   layout "blog"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
-  # GET /blogs
-  # GET /blogs.json
   def index
     @blogs = Blog.page(params[:page]).per(5)
     @page_title = "Blog"
     @seo_keywords = "Hannah Melton, Ruby on Rails, blog"
   end
 
-  # GET /blogs/1
-  # GET /blogs/1.json
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
+
     @page_title = @blog.title
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
@@ -40,8 +35,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /blogs/1
-  # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
@@ -52,8 +45,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
   def destroy
     @blog.destroy
     respond_to do |format|
