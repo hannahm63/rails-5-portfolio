772bc20371f2c9c7766d680b97cc06f0c7c96861
nikola <hannah.melton63@gmail.com>
2020-04-21 11:02:05 -0700

    implmented initial connection to Twitter API

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,8 @@ Bundler.require(*Rails.groups)
 
 module Rails5Portfolio
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.eager_load_paths << "#{Rails.root}/lib"
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
